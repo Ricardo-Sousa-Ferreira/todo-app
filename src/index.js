17c28df906fcfd1506468088401535f8677f31d2
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-24 11:43:48 +0000

    Add newTaskObject.js, object factory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("working");
+import {createObjectTask} from "./assets/newTaskObject.js";
+
+const Ricardo = createObjectTask("Ricardo", "macho", "21/02/1992", "low", "true", "12");
+console.log(Ricardo)
